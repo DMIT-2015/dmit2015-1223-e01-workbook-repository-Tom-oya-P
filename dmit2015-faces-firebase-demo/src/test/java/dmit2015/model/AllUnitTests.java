510ff54c4ce0e8a7dc1c56e211f67a4130c74a9f
Tom-oya-P <tphong1@nait.ca>
2023-05-29 20:10:16 -0600

    Day 10 / May 29 - JUnit and Test suite and rectangle example

---
+++ dmit2015-faces-firebase-demo/src/test/java/dmit2015/model/AllUnitTests.java
@@ -0,0 +1,14 @@
+package dmit2015.model;
+
+import org.junit.platform.suite.api.*;
+
+@Suite
+@SuiteDisplayName("JUnit 5 Platform Suite")
+@SelectPackages("ca.nait.dmit.domain")
+@IncludeClassNamePatterns(".*Test")
+@SelectClasses({CircleTest.class, RectangleTest.class})
+class AllUnitTests
+{
+// the class remains completely empty,
+// being used only as a holder for the above annotations
+}
