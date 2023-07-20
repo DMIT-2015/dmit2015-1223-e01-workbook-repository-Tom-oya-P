57c00775d4d122699ad9a15d305547033522dc74
Tom-oya-P <tphong1@nait.ca>
2023-07-19 21:17:00 -0600

    July 19 - Batch processing start

--- dmit2015-jaxrs-client-demo/src/main/java/dmit2015/faces/OpenWeatherApiClientView.java
+++ dmit2015-jaxrs-client-demo/src/main/java/dmit2015/faces/OpenWeatherApiClientView.java
@@ -34,55 +34,55 @@ public class OpenWeatherApiClientView implements Serializable {
     @ConfigProperty(name = "org.openweathermap.api.Units", defaultValue = "metric") // The name is defined in src/main/resources/META-INF/microprofile-config.properties file or an O/S environment variable
     private String units;    // // The units of measurement: standard, metric, imperial
 
-    @Inject
-    @RestClient // Inject the MP Rest Client
-    private CurrentWeatherApiResponseMpRestClient _weatherRestClient;
-
-    @Getter
-    private CurrentWeatherApiResponse currentWeatherApiResponse;
-
-    @Inject
-    @ConfigProperty(name = "IP_GEOLOCATION_API_KEY")
-    private String ipGeoLocationApiKey;
-
-    @Inject
-    @RestClient
-    private IpGeoLocationApiResponseMpRestClient _ipGeoLocationRestClient;
-
-    @Getter
-    private IpGeoLocationApiResponse ipGeoLocationResponse;
-
-    @PostConstruct
-    public void init() {
-        String ipAddress = Faces.getRemoteAddr();
-        ipGeoLocationResponse = _ipGeoLocationRestClient.getGeoLocation(ipGeoLocationApiKey, ipAddress);
-        city = ipGeoLocationResponse.getCity();
-
-        try {
-            currentWeatherApiResponse = _weatherRestClient.getCurrentWeatherByGeographicalCoordinates(
-                    ipGeoLocationResponse.getLatitude(),
-                    ipGeoLocationResponse.getLongitude(),
-                    openweatherApiKey,
-                    units);
-            String message = String.format("The current weather in %s is %d", currentWeatherApiResponse.getName(), currentWeatherApiResponse.getMain().getTemp());
-            Messages.addGlobalInfo(message);
-        } catch (Exception ex) {
-            ex.printStackTrace();
-            Messages.addGlobalWarn("There are no weather data for {0}", city);
-        }
-
-//        doSearchByCity();
-    }
-
-    public void doSearchByCity() {
-        try {
-            currentWeatherApiResponse = _weatherRestClient.getCurrentWeatherByCity(city, openweatherApiKey, units);
-            String message = String.format("The current weather in %s is %d", currentWeatherApiResponse.getName(), currentWeatherApiResponse.getMain().getTemp());
-            Messages.addGlobalInfo(message);
-        } catch (Exception ex) {
-            ex.printStackTrace();
-            Messages.addGlobalWarn("There are no weather data for {0}", city);
-        }
-    }
+//    @Inject
+//    @RestClient // Inject the MP Rest Client
+//    private CurrentWeatherApiResponseMpRestClient _weatherRestClient;
+//
+//    @Getter
+//    private CurrentWeatherApiResponse currentWeatherApiResponse;
+//
+//    @Inject
+//    @ConfigProperty(name = "IP_GEOLOCATION_API_KEY")
+//    private String ipGeoLocationApiKey;
+//
+//    @Inject
+//    @RestClient
+//    private IpGeoLocationApiResponseMpRestClient _ipGeoLocationRestClient;
+//
+//    @Getter
+//    private IpGeoLocationApiResponse ipGeoLocationResponse;
+//
+//    @PostConstruct
+//    public void init() {
+//        String ipAddress = Faces.getRemoteAddr();
+//        ipGeoLocationResponse = _ipGeoLocationRestClient.getGeoLocation(ipGeoLocationApiKey, ipAddress);
+//        city = ipGeoLocationResponse.getCity();
+//
+//        try {
+//            currentWeatherApiResponse = _weatherRestClient.getCurrentWeatherByGeographicalCoordinates(
+//                    ipGeoLocationResponse.getLatitude(),
+//                    ipGeoLocationResponse.getLongitude(),
+//                    openweatherApiKey,
+//                    units);
+//            String message = String.format("The current weather in %s is %d", currentWeatherApiResponse.getName(), currentWeatherApiResponse.getMain().getTemp());
+//            Messages.addGlobalInfo(message);
+//        } catch (Exception ex) {
+//            ex.printStackTrace();
+//            Messages.addGlobalWarn("There are no weather data for {0}", city);
+//        }
+//
+////        doSearchByCity();
+//    }
+//
+//    public void doSearchByCity() {
+//        try {
+//            currentWeatherApiResponse = _weatherRestClient.getCurrentWeatherByCity(city, openweatherApiKey, units);
+//            String message = String.format("The current weather in %s is %d", currentWeatherApiResponse.getName(), currentWeatherApiResponse.getMain().getTemp());
+//            Messages.addGlobalInfo(message);
+//        } catch (Exception ex) {
+//            ex.printStackTrace();
+//            Messages.addGlobalWarn("There are no weather data for {0}", city);
+//        }
+//    }
 
 }
